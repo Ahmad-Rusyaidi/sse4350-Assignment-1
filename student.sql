8046525a13c335a8a36779d08e9429e4b881f68c
Ahmad-Rusyaidi <95043331+Ahmad-Rusyaidi@users.noreply.github.com>
2022-03-28 12:09:31 +0800

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,7 @@
+CREATE TABLE student(
+	id int primary key,
+    nameStudent varchar(100) not null,
+    department varchar(100) not null);
+insert into student values (207523, "Ahmad Rusyaidi", "Software Engineering");
+insert into student values (207525, "Abdul Qayyum", "Networking");
+insert into student values (208129, "Ahmad Zaimirul", "Multimedia");
